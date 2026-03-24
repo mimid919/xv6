8efbd5484ce76d29c9b744fb3a0d23f7fa81edf3
Amelia Dunbar <mimi@Amelias-MacBook-Air-5.local>
2026-03-25 11:22:59 +1300

    write the findppid user code

--- user/findppid.c
+++ user/findppid.c
@@ -1,7 +1,34 @@
-#include <stdlib.h>
+#include "kernel/types.h"
+#include "user/user.h"
+
 
 int main(int argc, char *argv[])
 {
-  // your code
+    char *errorMessage = "Usage: findppid <num>\n";
+
+    // print error to standard error if no number of child processes provided
+    if (argc < 2) {
+        write(2, errorMessage, strlen(errorMessage));
+        exit(1);
+    }
+
+    // convert number of child processes from string to integer
+    int numChildren = atoi(argv[1]);
+
+    // plan is to make a loop
+    for (int i = 0; i < numChildren; i++) {
+        int pid = fork();
+        if (pid > 0) { // parent process
+            wait(0); // wait for child to finish
+            // print the parent process ID
+            printf("my ID is %d, my parent ID is %d\n", pid, getppid());
+            exit(0);
+        }
+    }
+
+    //eldest child prints
+    printf("my ID is %d, my parent ID is %d\n", getpid(), getppid());
+
+  
   exit(0);
 }
