1a4b3da85bb419c0de6f5dee562cfc87b8392682
Amelia Dunbar <mimi@Amelias-MacBook-Air-5.local>
2026-03-25 16:01:17 +1300

    implement primes_sum.c

--- user/primes_sum.c
+++ user/primes_sum.c
@@ -2,45 +2,49 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     if (argc < 2) {
-        write(2, "Usage: primes <num>\n", 20);
+        write(2, "Usage: primesum <num>\n", 22);
         exit(1);
     }
 
     int n = atoi(argv[1]);
     if (n < 2 || n > 200) {
-        write(2, "for primes <num>, num must be between 2 and 200\n", 48);
+        write(2, "for primesum <num>, num must be between 2 and 200\n", 49);
         exit(1);
     }
 
     int pipe_fd[2];
     pipe(pipe_fd);
 
-    int prime;
-    int nextPrime;
-    int pid;
+    // Parent generates numbers 2..n
+    if (fork() == 0) {
+        close(pipe_fd[1]);       // close write end in child
+        int input = pipe_fd[0];  // read from pipe
 
-    pid = fork();
-    if (pid == 0) { 
-        close(pipe_fd[1]);
-        int input = pipe_fd[0];
+        int prev_prime = 0; // store previous prime for sum
 
-        while (read(input, &prime, sizeof(prime)) > 0) {
-            //read it again to get the next number
-            read(input, &nextPrime, sizeof(nextPrime));
-            printf("%d\n", prime + nextPrime);
+        while (1) {
+            int prime;
+            int r = read(input, &prime, sizeof(prime));
+            if (r <= 0) break; // no more numbers
 
+            if (prev_prime > 0) {
+                printf("%d\n", prev_prime + prime);
+            }
+            prev_prime = prime;
+
+            // set up next pipe
             int next_pipe[2];
             pipe(next_pipe);
 
-            int child_pid = fork();
-            if (child_pid == 0) {
-                close(next_pipe[1]);
+            if (fork() == 0) {
+                // child: reads from next_pipe
+                close(next_pipe[1]); 
                 close(input);
                 input = next_pipe[0];
             } else {
+                // parent: filter remaining numbers
                 close(next_pipe[0]);
                 int num;
                 while (read(input, &num, sizeof(num)) > 0) {
@@ -54,9 +58,11 @@ int main(int argc, char *argv[])
                 exit(0);
             }
         }
+
         close(input);
         exit(0);
     } else {
+        // Parent writes numbers
         close(pipe_fd[0]);
         for (int i = 2; i <= n; i++) {
             write(pipe_fd[1], &i, sizeof(i));
