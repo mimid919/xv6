bdba59eee2560f8a1931ff22a3ba8a87a9183626
Amelia Dunbar <mimi@Amelias-MacBook-Air-5.local>
2026-03-25 15:02:46 +1300

    following the instructions steps

--- user/primes.c
+++ user/primes.c
@@ -2,8 +2,7 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     if (argc < 2) {
         write(2, "Usage: primes <num>\n", 20);
         exit(1);
@@ -18,49 +17,40 @@ int main(int argc, char *argv[])
     int pipe_fd[2];
     pipe(pipe_fd);
 
+    for (int i = 2; i <= n; i++) {
+        write(pipe_fd[1], &i, sizeof(i));
+    }
+    close(pipe_fd[1]);
+
+    int input = pipe_fd[0];
     int prime;
-    int pid;
 
-    pid = fork();
-    if (pid == 0) {
-        close(pipe_fd[1]);
-        int input = pipe_fd[0];
+    while (read(input, &prime, sizeof(prime)) > 0) {
+        printf("%d\n", prime);
 
-        while (read(input, &prime, sizeof(prime)) > 0) {
-            printf("%d\n", prime);
+        int right_pipe[2];
+        pipe(right_pipe);
 
-            int next_pipe[2];
-            pipe(next_pipe);
-
-            int child_pid = fork();
-            if (child_pid == 0) {
-                close(next_pipe[1]);
-                close(input);
-                input = next_pipe[0];
-            } else {
-                close(next_pipe[0]);
-                int num;
-                while (read(input, &num, sizeof(num)) > 0) {
-                    if (num % prime != 0) {
-                        write(next_pipe[1], &num, sizeof(num));
-                    }
+        int pid = fork();
+        if (pid == 0) {
+            close(right_pipe[1]);
+            close(input);
+            input = right_pipe[0];
+        } else {
+            close(right_pipe[0]);
+            int num;
+            while (read(input, &num, sizeof(num)) > 0) {
+                if (num % prime != 0) {
+                    write(right_pipe[1], &num, sizeof(num));
                 }
-                close(next_pipe[1]);
-                close(input);
-                wait(0);
-                exit(0);
             }
+            close(right_pipe[1]);
+            close(input);
+            wait(0);
+            exit(0);
         }
-        close(input);
-        exit(0);
-    } else {
-        close(pipe_fd[0]);
-        for (int i = 2; i <= n; i++) {
-            write(pipe_fd[1], &i, sizeof(i));
-        }
-        close(pipe_fd[1]);
-        wait(0);
     }
 
+    close(input);
     exit(0);
 }
